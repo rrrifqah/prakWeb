c255eec8bcb895bc3e6c6b1a0207ea46c6646d74
Nanda <rrrifqah@gmail.com>
2026-05-10 07:33:35 +0800

    feat: add Category and Item models, controllers, migrations, and routes

---
+++ app/Models/Item.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    protected $fillable = ['name', 'description', 'quantity', 'price', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
