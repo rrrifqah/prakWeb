22f3dbe8a2ed9be2a6e773f2e2e25920962238db
Nanda <rrrifqah@gmail.com>
2026-05-30 12:51:37 +0800

    Add Laravel Sanctum auth and role middleware

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ItemController;
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('items', ItemController::class);
+// Route Autentikasi (public)
+Route::post('register', 'App\Http\Controllers\AuthController@register');
+Route::post('login',    'App\Http\Controllers\AuthController@login');
+
+// Route yang dilindungi Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', 'App\Http\Controllers\CategoryController')
+        ->except(['destroy']);
+
+    Route::delete('categories/{category}',
+        'App\Http\Controllers\CategoryController@destroy')
+        ->middleware('role:admin');
+
+    Route::apiResource('items', 'App\Http\Controllers\ItemController')
+        ->except(['destroy']);
+
+    Route::delete('items/{item}',
+        'App\Http\Controllers\ItemController@destroy')
+        ->middleware('role:admin');
+});
